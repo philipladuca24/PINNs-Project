7540fd9766bd71439bcf602e0dd23b13306f9cda
Philip LaDuca <philip_laduca@brown.edu>
2022-12-08 01:41:31 -0500

    working bfgs optimizers

--- Code/lambda_adapt_2.py
+++ Code/lambda_adapt_2.py
@@ -11,6 +11,9 @@ from jaxopt import ScipyMinimize
 from jax.scipy.optimize import minimize
 from functools import partial
 
+#pip install jaxopt
+import jaxopt
+
 # CLEAN UP CODE AND ADD DOCSTRINGS 
 
 def random_layer_params(m, n, key, scale):
@@ -88,59 +91,6 @@ def loss(params, X, nu, l_lb, l_ub):
     lossub = loss_ub(params)
     return jnp.sum(l_lb * losslb + l_ub * lossub + lossf)
 
-# @jit
-# def optimise_lbfgs(params, X_, nu_, lf_, lb_):
-#     """
-#     Computes gradients and applies the optimizer (L-BFGS-B) to the loss.
-#     The adaptive weights are only updated in the Adam training steps, 
-#     and are held constant during L-BFGS training.
-
-#     Args:
-#         params (Tracer of list[DeviceArray]): list containing weights and biases.
-#         X (Tracer of DeviceArray): Collocation points in the domain.
-#         nu (Tracer of float): _description_
-#         lambda_b (Tracer of DeviceArray): Self-adaptive weight for the boundary loss.
-#         lambda_f (Tracer of DeviceArray): Self-adaptive weight for the residue loss.
-
-#     Returns:
-#         Tuple[(Tracer of) DeviceArray]: Tuple of the optimised loss.
-#     """
-#     opt_params = minimize(fun=loss, x0=jnp.array([params]), args=(X_, nu_, lf_, lb_), method="BFGS")
-#     opt_params = opt_params.x
-#     return opt_params
-
-# @jit
-# def minimize(params, X, nu, lb, ub): 
-#     minimizer = ScipyMinimize(method="L-BFGS-B", fun=loss, jit=True)
-#     opt_params = minimizer.run(jnp.array([params]), X, nu, lb, ub)
-#     opt_params = opt_params.params
-
-#     return opt_params
-
-####### Hyperparameters ##################
-nu = 10 ** (-3)
-layer_sizes = [1, 20, 20, 20, 1]
-nIter = 20000 + 1
-params = init_network_params(layer_sizes, random.PRNGKey(0))
-lambda_lb = random.uniform(random.PRNGKey(0), shape=[1])
-lambda_ub = random.uniform(random.PRNGKey(0), shape=[1])
-opt_init, opt_update, get_params = optimizers.adam(5e-4)
-opt_state = opt_init(params)
-opt_init_lb, opt_update_lb, get_params_lb = optimizers.adam(5e-4)
-opt_state_lb = opt_init_lb(lambda_lb)
-opt_init_ub, opt_update_ub, get_params_ub = optimizers.adam(5e-4)
-opt_state_ub = opt_init_ub(lambda_ub)
-l_lb_list = []
-l_ub_list = []
-lf_list = []
-x = jnp.arange(-1, 1.05, 0.05)
-
-# we can try to increase the layer size or increase/ (decrease? this would put more focus on the boundry points)
-# the number of points being trained on, we can also try to include 1 in the arange, 
-# different weighting on the loss function may help, we can try glorot
-# initialization for the parameters, not sure how to be more careful with the boundry
-
-
 @jit
 def step_param(istep, opt_state, X, lb, ub):
     params = get_params(opt_state)
@@ -158,6 +108,53 @@ def step_ub(istep, params, X, lb, opt_state):
     ub = get_params_ub(opt_state)
     g = grad(loss, argnums=4)(params, X, nu, lb, ub)
     return opt_update_ub(istep, -g, opt_state)
+
+def param_flatten(params):
+    params_new = jnp.array([])
+    for m in range(4):
+        for y in range(2):
+            a = jnp.ravel(params[m][y])
+            params_new = jnp.concatenate([params_new,a])
+    params_new = jnp.array(params_new)
+    return params_new
+
+def param_reshape(params, sizes):
+    params_re = []
+    for m, n in zip(sizes[:-1], sizes[1:]):
+        a = params[:m*n]
+        a = jnp.reshape(a, (m, n))
+        b = params[m*n:m*n + n]
+        params = params[m*n + n :]
+        params_re.append((jnp.array(a),jnp.array(b)))
+    return params_re
+
+def loss_scipy(params, X, nu, l_lb, l_ub, sizes):
+    params_re = param_reshape(params, sizes)
+    return loss(params_re, X, nu, l_lb, l_ub)
+
+def minimize_lbfgs(params, X, nu, lb, ub, sizes): 
+    minimizer = jaxopt.LBFGS(fun=loss_scipy, jit=False)
+    opt_params = minimizer.run(params, X, nu, lb, ub, sizes)
+    opt_params = opt_params.params
+    return opt_params
+
+####### Hyperparameters ##################
+nu = 10 ** (-3)
+layer_sizes = [1, 20, 20, 20, 1]
+nIter = 20000 + 1
+params = init_network_params(layer_sizes, random.PRNGKey(0))
+lambda_lb = random.uniform(random.PRNGKey(0), shape=[1])
+lambda_ub = random.uniform(random.PRNGKey(0), shape=[1])
+opt_init, opt_update, get_params = optimizers.adam(5e-4)
+opt_state = opt_init(params)
+opt_init_lb, opt_update_lb, get_params_lb = optimizers.adam(5e-4)
+opt_state_lb = opt_init_lb(lambda_lb)
+opt_init_ub, opt_update_ub, get_params_ub = optimizers.adam(5e-4)
+opt_state_ub = opt_init_ub(lambda_ub)
+l_lb_list = []
+l_ub_list = []
+lf_list = []
+x = jnp.arange(-1, 1.1, 0.1)
 
 pbar = trange(nIter)
 
@@ -185,21 +182,31 @@ for it in pbar:
         lam_lb_list.append(lambda_lb)
         lam_ub_list.append(lambda_ub)
 
-params = minimize(params, x, nu, lambda_lb, lambda_ub)
+params_new = param_flatten(params)
+params_min = minimize_lbfgs(params_new, x, nu, lambda_lb, lambda_ub, layer_sizes)
+#params_min = minimize(loss_scipy, params_new, args=(x, nu, lambda_lb, lambda_ub, layer_sizes), method="BFGS")
 
 print(lambda_lb, lambda_ub)
-u_pred = vmap(predict, (None, 0))(params, x)
+params_mini = param_reshape(params_min, layer_sizes)
+#params_mini = param_reshape(params_min.x, layer_sizes)
+u_pred = vmap(predict, (None, 0))(params_mini, x)
 
 plt.plot(x, u_pred)
+
 # plt.plot(lam_lb_list)
 # plt.plot(lam_ub_list)
 # plt.plot(l_lb_list)
 # plt.plot(l_ub_list)
 # plt.plot(lf_list)
+
 plt.show()
+
+print(loss_lb(params), "loss lower")
+print(loss_ub(params), "loss upper")
+
+print(loss_lb(params_mini), "loss lower_l-bfgs")
+print(loss_ub(params_mini), "loss upper_l-bfgs")
 
 
 # np.save("ld_list.npy", np.array(lb_list), allow_pickle=True)
 # np.save("lf_list.npy", np.array(lf_list), allow_pickle=True)
-
-
